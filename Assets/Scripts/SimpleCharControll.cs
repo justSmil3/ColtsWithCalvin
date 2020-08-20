1be84dfd779ff987889a5df1f22ee614c54a13b4
Christian Lott <christian.lott@protonmail.com>
2020-08-20 20:45:39 +0200

    initial upload

---
+++ Assets/Scripts/SimpleCharControll.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleCharControll : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        if (!TryGetComponent<Rigidbody>(out rb))
+            Destroy(this);
+    }
+}
